c6adfe18be88dbdbe8e1c5f3eac519e4f560820a
Irina Saveleva <goyalion17@gmail.com>
2024-01-24 20:23:34 +0100

    clearInterval(id), timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
 
 export default function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+
   useEffect(
     function () {
-      setInterval(function () {
+      const id = setInterval(function () {
         dispatch({ type: "tick" });
       }, 1000);
+      return () => clearInterval(id);
     },
     [dispatch]
   );
 
-  return <div className="timer">{secondsRemaining}</div>;
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}:{seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
 }
